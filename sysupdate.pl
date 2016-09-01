1324755178c9529d13ae463f5e4b6328d59b8027
lokender <lokender111@gmail.com>
2016-09-01 15:48:11 +0530

    first commit

--- sysupdate.pl
+++ sysupdate.pl
@@ -3,8 +3,23 @@ print "This script is made for fully upgarde your system ";
 print "we are updating list of avialble updates";
 system("apt-get update");
 
+if ( $? == -1 )
+{
+  print "command failed: $!\n";
+}
+
 print "we are upgrading it";
 system("apt-get upgrade");
 
+if ( $? == -1 )
+{
+  print "command failed: $!\n";
+}
+
 print "we are performing dist upgrading it";
 system("apt-get dist-upgrade");
+
+if ( $? == -1 )
+{
+  print "command failed: $!\n";
+}
